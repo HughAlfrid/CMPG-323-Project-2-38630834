aeeb17533dd351b52e46222fc16653251929bb64
HughAlfrid <129861251+HughAlfrid@users.noreply.github.com>
2023-08-30 10:33:18 +0200

    changes

--- Project 2/Models/zaazrcmpg323sqlContext.cs
+++ Project 2/Models/zaazrcmpg323sqlContext.cs
@@ -25,7 +25,6 @@ namespace Project_2.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
                 optionsBuilder.UseSqlServer("Password=azr@nwu1;Persist Security Info=True;User ID=serveradmin;Initial Catalog=zaazrcmpg323sql;Data Source=zaazr-cmpg323-data.database.windows.net");
             }
         }
